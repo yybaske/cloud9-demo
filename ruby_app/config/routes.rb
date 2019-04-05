859d498347971e8acefc1bcafb1330c76a12038a
yoshimura <yusuke22bbc@gmail.com>
2019-04-05 03:04:32 +0000

    ruby消去

--- ruby_app/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
-  
-  # 標準的なルーティングを宣言する
-  resources :articles
-  
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
